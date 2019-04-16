ecea4d74f5a880c4dab547811f85a8a242f3e274
agustinavila <tinto.avila@gmail.com>
2019-04-16 04:03:24 -0300

    Practica 2. logica combinacional

---
+++ practica3.asm
@@ -0,0 +1,8 @@
+; en memoria se encuentran almacenados tres datos:
+; dato 1: binario MyS 13b total
+; dato 2: BCD 12b + 1b Signo SRC10
+; dato 3: SRC2 12b
+;
+; Se debe encontrar el mayor y verificar si se puede representar en SRC2 8b
+; se deben llevar 3 ejemplos que verifiquen  desbordes, con num positivos y negativos.
+
