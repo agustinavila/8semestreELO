7130ed9a02904cbc214fbfa533611345eaf0bf4a
Agustin Avila <tinto.avila@gmail.com>
2020-10-13 00:28:50 -0300

    Pequeños cambios de constantes y de tiempo de tx

--- Sistemas para control/Practica 1/Practica1.ino
+++ Sistemas para control/Practica 1/Practica1.ino
@@ -2,27 +2,28 @@
 #include <TimerOne.h>
 #include <PID_v1.h>
 
+#pragma region constantes
 // Definiciones de constantes
-#define tiempo_rx 100     //tiempo de RX
-#define tiempo_tx 10      //Tiempo de TX
-#define SALIDA_MAX 4.5    //Salida máxima del PID (luego se le resta la Zona Muerta del Motor si hace falta)
-#define TENSION_MAX 5.0   //Tensión máxima de salida del pwm
-#define ZONA_MUERTA 0.25  //ZONA MUERTA DEL MOTOR
-#define TOLERANCIA 1.0    //Tolerancia en grados
-#define REF_MAX 90.0      //posicion maxima en grados
-#define REF_MIN -90.0     //posicion minima en grados
-#define SENSOR_MAX 765.0  //1.78 medio -- 3.72 0.18 -- medidas de manera practica
-#define SENSOR_MIN 37.0   //Tambien medido de manera practica
+#define tiempo_rx 30    //tiempo de RX
+#define tiempo_tx 10     //Tiempo de TX
+#define SALIDA_MAX 4.5   //Salida máxima del PID (luego se le resta la Zona Muerta del Motor si hace falta)
+#define TENSION_MAX 5.0  //Tensión máxima de salida del pwm
+#define ZONA_MUERTA 1.2 //ZONA MUERTA DEL MOTOR
+#define TOLERANCIA 1.0   //Tolerancia en grados
+#define REF_MAX 90.0     //posicion maxima en grados
+#define REF_MIN -90.0    //posicion minima en grados
+#define SENSOR_MAX 755.0 //1.78 medio -- 3.72 0.18 -- medidas de manera practica
+#define SENSOR_MIN 37.0  //Tambien medido de manera practica
 #define SENSOR_MEDIO ((SENSOR_MAX - SENSOR_MIN) / 2.0 + SENSOR_MIN)
 #define POT2GRAD ((REF_MAX - REF_MIN) / (SENSOR_MAX - SENSOR_MIN)) //escalado de potenciómetro a grados
 #define Ts 1                                                       //Periodo de muestreo de 1 ms
+#pragma endregion
 
-//Definiciones de variables
-
+#pragma region variables
 //Definiciones para el PID
 double setpoint = 0.0, entrada = 0.0, salida = 0.0;
 //Parametros PID
-float Kp = 0.5, Ki = 0.1, Kd = 0.01;
+float Kp = 1.2, Ki = 0.3, Kd = 0.01;
 //Variables leídas y error entre ellas
 float ref = 0.0, sensor = 0.0, error = 0.0;
 int pot = 0;
@@ -40,11 +41,14 @@ char buffer_rx[20];
 const byte numChars = 20; //
 char receivedChars[20];   // Arreglo donde se guardan los bytes recibidos
 float dataNumber = 0.0;
+#pragma endregion
 
+#pragma region inicio_objetos
 //Declaracion del objeto del motor
 AF_DCMotor motor(4, MOTOR34_8KHZ); //define la frecuencia de la señal PWM
 //Declaracion del objeto del PID
 PID myPID(&entrada, &salida, &setpoint, Kp, Ki, Kd, DIRECT);
+#pragma endregion
 
 void setup()
 {
@@ -65,9 +69,9 @@ void loop()
 {
   if (bandera_control == 1) // Lazo de control
   {
+    #pragma region sensado
     ref = dataNumber; //lee referencia de HyperIMU ([-90,90] en este caso, actualizada en "dataNumber" cada 50ms)
                       // ref= ((float)analogRead(A2)- SENSOR_MEDIO)*POT2GRAD; //Para usar otro pote de ref
-
     if (ref > REF_MAX)
     { //opcional, generalmente se utilizan fines de carrera
       ref = REF_MAX;
@@ -80,7 +84,9 @@ void loop()
     sensor = ((float)pot - SENSOR_MEDIO) * POT2GRAD; //valor actual en grados
     //se pueden agregar filtros digitales a "ref" y "sensor"
     error = ref - sensor; //Calculo de error
+    #pragma endregion
 
+    #pragma region control
     if (abs(error) < TOLERANCIA) //para parar el motor cuando el error es menor a la tolerancia
     {
       motor.run(RELEASE);
@@ -103,8 +109,10 @@ void loop()
     pwm_motor = (byte)((abs(salida) + ZONA_MUERTA) * (255.0 / SALIDA_MAX)); //mapeo de tension [volt] a PWM de 8 bits
     motor.setSpeed(pwm_motor);                                              //establece el duty (porcentaje de tiempo en "1") de la señal PWM
     bandera_control = 0;
+    #pragma endregion
   }
 
+  #pragma region comunicacion_serial
   //Chequeo para transmision y recepcion
   if (bandera_tx == 1)
   {
@@ -117,23 +125,21 @@ void loop()
     recvWithEndMarker();
     bandera_rx = 0;
   }
+  #pragma endregion
 }
 
-//rutina (función) llamada cada interrupción del timer (1 ms)
 void timerIsr()
 {
-  bandera_control = 1;    //activa tarea lazo de control cada 1 ms
+  bandera_control = 1; //activa tarea lazo de control cada 1 ms
   contador_tx++;
   contador_rx++;
-
-  if (contador_tx >= tiempo_tx)  //activa tarea transmisión de datos cada 10 ms
-  { 
+  if (contador_tx >= tiempo_tx) //activa tarea transmisión de datos cada 10 ms
+  {
     bandera_tx = 1;
     contador_tx = 0;
   }
-
   if (contador_rx >= tiempo_rx) //activa tarea recepción de datos cada 50 ms
-  { 
+  {
     bandera_rx = 1;
     contador_rx = 0;
   }
